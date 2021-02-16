ee1802a84f07c9cf3ef8aec1debb58801f3c071c
Mauricio <mjunior447@gmail.com>
2021-02-15 21:55:10 -0300

    criadas funcionalidades iniciais de passar os dados do formulário para um JSON e usá-lo para alimentar a tabela;

---
+++ index.js
@@ -0,0 +1,52 @@
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
+var tbody = document.getElementById("table-users");
+
+// criar linhas na tabela com os dados do formulário
+function addLine(userData) {
+
+    var tr = document.createElement("tr");
+
+    tbody.innerHTML += `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.admin}</td>
+        <td>${user.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>
+    `;
+    
+}
+
+// tratando o envio de dados do formulário
+document.getElementById("form-user-create").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // preenchendo o JSON com os dados do formulário
+    fields.forEach((field) => {
+        // if (field.name == "gender") {
+        //     if (field.checked) {
+        //         user[field.name] = field.value;
+        //     }
+        // } else {
+        //     user[field.name] = field.value;
+        // }
+
+        if (field.name == "gender") {
+            if (field.checked) {
+                user[field.name] = field.value;
+            }
+        } else if (field.name == "admin") {
+            user[field.name] = field.checked;
+        } else {
+            user[field.name] = field.value;
+        }
+    });
+
+    addLine(user);
+})
