fed807412753517944cbe1fc4247088f7f36d391
Mauricio <mjunior447@gmail.com>
2021-02-16 21:26:25 -0300

    processamento da imagem feito agora através de Promises

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -13,9 +13,11 @@ class UserController {
             event.preventDefault();
             let values = this.getValues();
 
-            this.getPhoto((content) => {
+            this.getPhoto().then((content) => {
                 values.photo = content;
                 this.addLine(values);
+            }, (e) => {
+                console.error(e);
             });
 
         })
@@ -23,22 +25,30 @@ class UserController {
 
 
     // método que trata do recebimento da foto pelo javascript
-    getPhoto(callback) {
-        let fileReader = new FileReader();
+    getPhoto() {
 
-        let elements = [...this.formEl.elements].filter(item => {
-            if (item.name === "photo") {
-                return item;
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item => {
+                if (item.name === "photo") {
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
             }
+
+            fileReader.onerror = e => {
+                reject(e);
+            }
+
+            fileReader.readAsDataURL(file);
         });
 
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-            callback(fileReader.result);
-        }
-
-        fileReader.readAsDataURL(file);
     }
 
 
