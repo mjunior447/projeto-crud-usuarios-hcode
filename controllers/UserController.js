cb0fc4d1c747d16ef26654a9553e1369a1a2665e
Mauricio <mjunior447@gmail.com>
2021-07-05 12:47:23 -0300

    validação de formulário no javascript; campos 'nome', 'email' e 'password' marcados como 'required' no HTML

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,6 +16,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then((content) => {
                 values.photo = content;
                 this.addLine(values);
@@ -24,6 +26,7 @@ class UserController {
             }, (e) => {
                 console.error(e);
             });
+
 
         })
     }
@@ -65,7 +68,18 @@ class UserController {
     // retornando uma instância da classe User com esses dados
     getValues() {
         let user = {};
-            [...this.formEl.elements].forEach((field) => {
+        let isValid = true;
+        [...this.formEl.elements].forEach((field) => {
+
+            // validação de formulário
+            // os 3 campos no array sao obrigatorios
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
+
             if (field.name == "gender") {
                 if (field.checked) {
                     user[field.name] = field.value;
@@ -76,6 +90,10 @@ class UserController {
                 user[field.name] = field.value;
             }
         });
+
+        if (!isValid) {
+            return false;
+        }
 
         return new User(
             user.name,
@@ -109,7 +127,14 @@ class UserController {
         `;
 
         this.tableEl.appendChild(tr);
+        this.updateCount();
         
+    }
+
+    updateCount() {
+
+        console.dir(this.tableEl);
+
     }
 
 }
