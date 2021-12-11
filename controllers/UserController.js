4f43bfaccd11e7b2ee5e31358723b55393206ae5
Mauricio <mjunior447@gmail.com>
2021-12-11 16:57:19 -0300

    botão editar agora abre outro formulário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,6 +5,16 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEditCancel();
+    }
+
+    // cria o evento de cancelar a edição de linha da tabela de usuários
+    onEditCancel() {
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+            this.showPanelCreate();
+        });
+
     }
 
     // trata do evento de envio de dados do formulário
@@ -124,15 +134,47 @@ class UserController {
             <td>${(userData.admin) ? "Sim" : "Não"}</td>
             <td>${Utils.dateFormat(userData.register)}</td>
             <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>
         `;
 
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+
+            for(let name in json) {
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+
+                if(field) {
+                    if(field.type === "file") continue;
+                    field.value = json[name];
+                }
+            }
+
+            this.showPanelUpdate();
+
+        });
+
         this.tableEl.appendChild(tr);
         this.updateCount();
         
+    }
+
+    showPanelCreate() {
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+    showPanelUpdate() {
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+
     }
 
     updateCount() {
